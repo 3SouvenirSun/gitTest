51e5449e99d35003a0c9aeab1f72b3736e70f975
Souvenir <3150795436@qq.com>
2024-03-03 08:27:04 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitT.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: gitT
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author Souvenir
+ * @Create 2024/3/3 8:08
+ * @Version 0.0
+ */
+public class gitT {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
